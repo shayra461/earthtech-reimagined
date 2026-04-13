84649c43eeee32161b0942658526922b401aea77
Peterwood <156454307+shayra461@users.noreply.github.com>
2026-04-14 04:41:15 +0500

    Set base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/earthtech-reimagined/",
   server: {
     host: "::",
     port: 8080,
